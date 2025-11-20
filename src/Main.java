d7b09cb6a61c22bb397f28e0035cb613750bb9b5
Jesús Samuel Zavala Ayala <jsamuelzavalay@gmail.com>
2025-11-20 09:26:52 -0600

    la cosa para leer archivos ha llegado

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Lector l = new Lector();
         ArrayList<String> cuadruplos = l.lineas("src/archivo.txt");
+        System.out.println(cuadruplos.size());
     }
 }
