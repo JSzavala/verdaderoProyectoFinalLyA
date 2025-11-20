118b4113e13da105855f2e29d82464709976224b
Jesús Samuel Zavala Ayala <jsamuelzavalay@gmail.com>
2025-11-20 11:24:06 -0600

    mas cositas jijiji

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,13 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Lector l = new Lector();
+        Validador v = new Validador();
+        Optimizador o = new Optimizador();
         ArrayList<String> cuadruplos = l.lineas("src/archivo.txt");
-        System.out.println(cuadruplos.size());
+        try{
+            //v.validar(cuadruplos);
+        } catch (InvalidFormatException e) {
+            System.err.println("Los cuadruplos no tienen un formato valido" + e.getMessage());
+        }
     }
 }
